30ecb04bbb1b910cd6bf4094dc8376451922fe9d
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-07-19 17:50:27 -0600

    Working on login page.

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,13 +1,15 @@
 import checkLogin from "./utilities/checkLogin";
 import {standardBackendResponse} from "./utilities/interfaces";
+import {Dispatch, SetStateAction, useState} from "react";
 let cc = console.log;
-checkLogin().then((response) => handleCheckLogin(response));
 
-function handleCheckLogin(response: standardBackendResponse){
-    cc(response)
+function handleCheckLogin(response: standardBackendResponse, setLoginState: Dispatch<SetStateAction<boolean>>){
+    cc(response.data)
 }
 
 function Login(){
+    let [loginState, setLoginState] = useState(false);
+    checkLogin().then((response) => handleCheckLogin(response, setLoginState));
 
     return (
         <div>
