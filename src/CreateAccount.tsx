e4dda78b664350459ae28a1d259731888a21a2db
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-08-02 01:21:10 -0600

    added form for  account creation

--- src/CreateAccount.tsx
+++ src/CreateAccount.tsx
@@ -1,8 +1,41 @@
+import {useState} from "react";
+
 function CreateAccount(){
+    const [usernameState, setUsernameState] = useState("");
+    const [emailAddressState, setEmailAddressState] = useState("");
+    const [emailAddressVerifyState, setEmailAddressVerifyState] = useState("");
+    const [passwordState, setPasswordState] = useState("");
+    const [passwordVerifyState, setPasswordVerifyState] = useState("");
+
     return (
-        <>
-            test
-        </>
+        <div>
+            <form>
+                <input type={"text"} value={usernameState} onChange={(e) => {
+                    setUsernameState(e.target.value);
+                }}/>
+                <br />
+                <input type={"text"} value={emailAddressState} onChange={(e) => {
+                    setEmailAddressState(e.target.value);
+                }}/>
+                <br />
+                <input type={"text"} value={emailAddressVerifyState} onChange={(e) => {
+                    setEmailAddressVerifyState(e.target.value);
+                }}/>
+                <br />
+                <input type={"password"} value={passwordState} onChange={(e) => {
+                    setPasswordState(e.target.value);
+                }}/>
+                <br />
+                <input type={"password"} value={passwordVerifyState} onChange={(e) => {
+                    setPasswordVerifyState(e.target.value);
+                }}/>
+                <br />
+                <button onClick={(e) => {
+                    e.preventDefault();
+
+                }}>Submit</button>
+            </form>
+        </div>
     );
 }
 
