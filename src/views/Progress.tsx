d336d53d56224f07521dc33565da6390103f36b7
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-08-07 01:09:19 -0600

    Installed react-activity-calendar

--- src/views/Progress.tsx
+++ src/views/Progress.tsx
@@ -1,7 +1,38 @@
+import Chart from "react-apexcharts";
+
 function Progress(){
 
+    let mock1RM = [120, 140, 140, 140, 140, 145];
+    let mocklDates = [""];
+
     return (
-        <></>
+        <div className={"progressContainer"}>
+            <div className={"options"}>
+                test
+            </div>
+
+            <div className={"chartContainer"}>
+                <Chart
+                    series = {[
+                        {
+                            data: mock1RM
+                        }
+                    ]}
+                    type="bar"
+                    height={400}
+                    options = {{
+                        plotOptions: {
+                            bar: {
+                                horizontal: true,
+                            }
+                        },
+                        xaxis: {
+                            //categories: job.yearsNumbered,
+                        },
+                    }}
+                />
+            </div>
+        </div>
     )
 }
 
