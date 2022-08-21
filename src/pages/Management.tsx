7b1ac4e5b72d908456a2b4cf0b8d6729dc50b167
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-08-21 15:13:08 -0600

    Added nav and started hooks.

--- src/pages/Management.tsx
+++ src/pages/Management.tsx
@@ -1,6 +1,19 @@
+import {useState} from "react";
+import Nav from "../components/Nav";
+
 function Management(){
+    const [notesListState, setNotesListState] = useState<string[]>([""]);
+    const [notesSessionSelectorState, setNotesSessionSelectorState] = useState<string>("");
+    const [notesSessionState, setNotesSessionState] = useState<any>();
+
+
     return (
-        <></>
+        <div>
+            <Nav title={"View & Manage Session Data"}/>
+            <div className={"basicContainer"}>
+                test
+            </div>
+        </div>
     )
 }
 
