9c4abd3ccedb4aa01d0a1d26de6f17e7776d18b6
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-08-25 17:15:25 -0600

    TextFieldReusable placeholder fix.

--- src/pages/createaccount/TextFieldReusable.tsx
+++ src/pages/createaccount/TextFieldReusable.tsx
@@ -5,7 +5,7 @@ function TextFieldReusable({state, setState, placeholder, type}:
                            {state: string, setState: Dispatch<SetStateAction<string>>, placeholder: string, type: string}){
     return (
         <TextField type={type} variant={"standard"} sx={{"display": "block", "marginBottom": "8px"}}
-           value={state} placeholder={"Username"}
+           value={state} placeholder={placeholder}
            onChange={(e) => {
                e.preventDefault();
                setState(e.target.value);
